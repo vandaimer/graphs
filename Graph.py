c5f8cd36ed26cc561c35a9a46248b14b13e80624
Luiz Filipe <moresco.luiz@gmail.co>
2015-11-10 20:38:23 -0200

    Adicionado comentários nos métodos

--- Graph.py
+++ Graph.py
@@ -7,15 +7,17 @@ class Graph:
         self.graph = {}
         self.lastVertex = None
 
+    #adiciona um vértice, se o vertice já existe, retorna falso
     def addVertex( self, vertexName ):
-        if self.graph.get( vertexName ) != None:
+        if self._existVertex( {vertexName} ) != False:
             return False
         self.graph[vertexName]  = {}
         self.lastVertex         = vertexName
         return True
 
+    #remove um vértice, se não existe retorna falso
     def removeVertex( self, vertexName ):
-        if self.graph.get( vertexName ) == None:
+        if self._existVertex( { vertexName } ) == False:
             return False
         del self.graph[vertexName]
         for vertex in self.graph:
@@ -27,31 +29,41 @@ class Graph:
             setOfVertex = tuple( self.graph )
             self.lastVertex = setOfVertex[len(setOfVertex)-1]
 
+    #desconecta dois vertices, caso um ou os dois não exista, ele retorna falso
     def desconnect( self, vertexA, vertexB ):
         if self._existVertex( {vertexA,vertexB} ) == False:
             return False
         del self.graph[vertexA][vertexB]
         return True
 
+    #conecta dois vertices, caso um ou os dois não exista, ele retorna falso
     def connect( self, vertexA,vertexB,label = None ):
         if self._existVertex( {vertexA,vertexB} ) == False:
             return False
         self.graph[vertexA][vertexB] = label
 
+    #Retorna o número de vértices do grafo
     def order( self ):
         return len( self.graph )
 
+    #Retorna o número de vertices adjacentes de um vertice, se o vertice
+    #passado não existir, retorna falso
     def degree( self,vertex ):
         if self._existVertex( {vertex} ) == False:
             return False
         return len(self.graph[vertex])
 
+    #retorna todos os vertices do grafo
     def getRelationVertex( self ):
-        return self.graph
+        return self.graph.keys()
 
+    #retorna um vertice "qualquer", que nesse caso é sempre o último
+    #adicionado
     def anyVertex( self ):
         return self.lastVertex
 
+    #Retorna os adjacentes de um vertice, se o vertice passado não existir
+    #retorn falso
     def related( self,vertex ):
         if self._existVertex( { vertex } ) == False:
             return False
